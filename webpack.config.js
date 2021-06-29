29d74465d557fc123c945632a57abae803e10757
Shea Cronin <shealcronin@gmail.com>
2021-06-29 18:00:32 -0400

    Add HTMLWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Battleship',
+          template: './src/index.html'
+        }),
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     mode: 'development',
     module: {
