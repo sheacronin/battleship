d53031e4f219a30cbe9519b8ef739c08bcf31340
Shea Cronin <shealcronin@gmail.com>
2021-09-18 13:27:42 -0400

    Add enemyBoard property to Player object for it to attack

--- src/player.js
+++ src/player.js
@@ -1,7 +1,8 @@
 class Player {
-    constructor(name) {
+    constructor(name, enemyBoard) {
         this.name = name;
         this.isComputer = name === 'Computer';
+        this.enemyBoard = enemyBoard;
         this.isMyTurn = false;
     }
 
@@ -9,14 +10,17 @@ class Player {
         this.isMyTurn = !this.isMyTurn;
     }
 
-    attack() {
-        if (this.isComputer) {
-            return [
-                Math.floor(Math.random() * 10),
-                Math.floor(Math.random() * 10),
-            ];
+    attack(x, y) {
+        if (!this.enemyBoard) {
+            throw new Error('player must have an enemy board to attack');
         }
-        return [0, 0];
+
+        if (this.isComputer) {
+            x = Math.floor(Math.random() * 10);
+            y = Math.floor(Math.random() * 10);
+        }
+        this.enemyBoard.receiveAttack(x, y);
+        return [x, y];
     }
 }
 
--- src/player.test.js
+++ src/player.test.js
@@ -1,9 +1,16 @@
 /* eslint-disable no-undef */
 import Player from './player';
 
+const mockBoard = jest.fn(() => {
+    const receiveAttack = jest.fn();
+    return {
+        receiveAttack,
+    };
+});
+
 test('player can attack', () => {
-    const player = new Player();
-    expect(player.attack()).toStrictEqual([0, 0]);
+    const player = new Player('Bob', new mockBoard());
+    expect(player.attack(0, 0)).toStrictEqual([0, 0]);
 });
 
 test('player is computer if name === "Computer"', () => {
@@ -15,7 +22,7 @@ test('player with any other name is not a computer', () => {
 });
 
 test('computer player returns valid random attack coordinates', () => {
-    const computer = new Player('Computer');
+    const computer = new Player('Computer', new mockBoard());
     const [x, y] = computer.attack();
     expect(x).toBeGreaterThanOrEqual(0);
     expect(x).toBeLessThan(10);
@@ -45,4 +52,25 @@ test('player can switch to it not being their turn', () => {
     expect(player.isMyTurn).toBe(true);
     player.switchTurn();
     expect(player.isMyTurn).toBe(false);
+});
+
+test('player has a enemyBoard property', () => {
+    const exampleEnemyBoard = new mockBoard();
+    expect(new Player('Computer', exampleEnemyBoard).enemyBoard).toBe(
+        exampleEnemyBoard
+    );
+});
+
+test('on player attack, enemyBoard.receiveAttack runs', () => {
+    const exampleEnemyBoard = new mockBoard();
+    const player = new Player('Computer', exampleEnemyBoard);
+    player.attack();
+    expect(exampleEnemyBoard.receiveAttack).toHaveBeenCalled();
+});
+
+test('throws an error when trying to attack without an enemyBoard assigned', () => {
+    const player = new Player('Computer');
+    expect(() => player.attack()).toThrow(
+        'player must have an enemy board to attack'
+    );
 });
